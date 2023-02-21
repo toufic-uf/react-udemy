f26d99e9ef1d78a36e6081b3bea192aaf9ec9145
Toufic Nabi <toufic.nabi@uberflip.com>
2023-02-20 21:30:54 -0500

    Cocktail project

--- 15-cocktails/setup/src/components/SearchForm.js
+++ 15-cocktails/setup/src/components/SearchForm.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import {useRef, useEffect} from 'react'
 import { useGlobalContext } from '../context'
 
 const SearchForm = () => {
+  const { setSearchTerm } = useGlobalContext();
+  // console.log('testing', setSearchTerm);
+  const searchValue = useRef('')
+
+  useEffect(() => {
+    searchValue.current.focus()
+  }, [])
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  }
+  const handleSubmit = e => {
+    e.preventDefault()
+  }
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
+    <section className='section search'>
+      <form className='search-form' onSubmit={handleSubmit}>
+        <div className='form-control'>
+          <label htmlFor='name'>search your favorite cocktail</label>
+          <input type='text' name='name' id='name' ref={searchValue} onChange={searchCocktail} />
+        </div>
+      </form>
+    </section>
   )
 }
 
-export default SearchForm
+export default SearchForm;
